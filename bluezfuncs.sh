0cfebd2a33e96ab0d49c3c9ff3d855f583cd7eb8
Jorge Luis de la Torre <jorgedlt@users.noreply.github.com>
2025-08-26 13:45:03 -0400

    Update bluezfuncs.sh
    getting codex to work

--- bluezfuncs.sh
+++ bluezfuncs.sh
@@ -4,6 +4,8 @@
 
 # Save as ~/.ibmfuncs.sh then: source ~/.ibmfuncs.sh
 
+set -euo pipefail # needed for codex
+
 # Minimal ANSI colors
 RESET=$'\033[0m'; BOLD=$'\033[1m'
 RED=$'\033[31m'; GREEN=$'\033[32m'; YELLOW=$'\033[33m'
